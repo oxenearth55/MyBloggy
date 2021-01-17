b03cf1ca67589f5235b0a5f9da80b2aef0ad4b9c
Macintosh HD <>
2021-01-18 02:02:00 +0700

    .

--- middleware/multer.js
+++ middleware/multer.js
@@ -2,7 +2,9 @@ const multer = require('multer');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, './uploads/')
+        // cb(null, './uploads/')
+        cb(null, path.join(__dirname,".","uploads",yourfilename))
+
     },
     filename: function (req, file, cb) {
         cb(null, new Date().toISOString().replace(/:/g, '-') + file.originalname)
